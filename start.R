283a0c90f8b372d36a0e2ee3f6a18f2fcbc81b10
Richard Morey <richarddmorey@gmail.com>
2022-11-12 09:40:08 +0000

    initial commit

---
+++ start.R
@@ -0,0 +1,27 @@
+# If package cli is installed - the code below fails.
+# If package cli is not installed, it works. 
+install.packages('cli')
+# remove.packages('cli')
+library(RestRserve)
+library(promises)
+
+app = Application$new()
+
+tf = tempfile(fileext = '.qmd')
+cat('**hi**\n', file = tf)
+
+app$add_get(
+  path = "/a", 
+  FUN = function(.req, .res) {
+    rmarkdown::render(
+      input = tf,
+      output_format = 'html_fragment',
+      quiet = TRUE
+    )
+    .res$set_content_type("application/json")
+    .res$set_body(list())
+  })
+
+backend = BackendRserve$new()
+backend$start(app, http_port = 8080)
+
